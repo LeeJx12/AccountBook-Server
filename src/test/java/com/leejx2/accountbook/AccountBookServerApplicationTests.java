6d05b7bce95dfa05464a91f2e95a4e0a26b9c094
LeeJx2 <46637231+LeeJx12@users.noreply.github.com>
2022-04-23 14:13:08 +0900

    Feat: 개인가계부 서버 초기세팅
    웹서버, Rest API 구현할 서버 초기화 (Spring Boot 2.5.14)

---
+++ src/test/java/com/leejx2/accountbook/AccountBookServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leejx2.accountbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountBookServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
